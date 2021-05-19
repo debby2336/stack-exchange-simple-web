f164f7a454219df92719e4763b922bf490e79966
Debby Wong <szewingwong2336@gmail.com>
2021-05-19 22:17:51 +0800

    feat: search on change tag

--- src/modules/trending/containers/TrendingTagList.tsx
+++ src/modules/trending/containers/TrendingTagList.tsx
@@ -2,8 +2,9 @@ import Box from '@material-ui/core/Box'
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles'
 import * as R from 'ramda'
 import { useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
+import { questionsActionCreators } from '@redux/questions'
 import { getTagList } from '@redux/tags'
 
 import { TagItemType } from 'src/types/tags'
@@ -24,13 +25,14 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const TrendingTagList = () => {
   const classes = useStyles()
+  const dispatch = useDispatch()
   const tagList = useSelector(getTagList) as TagItemType[]
   const [selectedTagIndex, setSelectedTagIndex] = useState(0)
 
-  const handleChipClick = (selectedIndex: number) => {
+  const handleChipClick = (tagName: string, selectedIndex: number) => {
     if (selectedIndex !== selectedTagIndex) {
-      // TODO
       setSelectedTagIndex(selectedIndex)
+      dispatch(questionsActionCreators.fetchQuestionList(1, 20, tagName))
     }
   }
 
@@ -41,7 +43,7 @@ const TrendingTagList = () => {
           active={index === selectedTagIndex}
           key={index}
           tag={tag.name}
-          handleChipClick={() => handleChipClick(index)}
+          handleChipClick={() => handleChipClick(tag.name, index)}
         />
       ))(tagList)}
     </Box>
